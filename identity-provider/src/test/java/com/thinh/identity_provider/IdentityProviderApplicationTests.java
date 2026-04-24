4f68ea2f790f26a5c4b9ef44f265c0f36fcc4190
thinh <anvanthinh@gmail.com>
2026-04-24 23:42:21 +0700

    add identity-provider

---
+++ identity-provider/src/test/java/com/thinh/identity_provider/IdentityProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.identity_provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
